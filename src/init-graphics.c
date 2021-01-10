1c778904b549c1ccc00784525e2e3b87f1e1b00a
wdlkmpx <wdlkmpx@gmail.com>
2021-01-10 20:22:32 +0800

    remove bitmaps.h
    include them directly in init-graphics.c

--- src/init-graphics.c
+++ src/init-graphics.c
@@ -3,7 +3,27 @@
 /*                                   */
 
 #include "graphics.h"
-#include "bitmaps.h"
+
+#include "./bitmaps/star.xpm"
+
+#include "./bitmaps/block1.xpm"
+#include "./bitmaps/block2.xpm"
+#include "./bitmaps/block3.xpm"
+#include "./bitmaps/block4.xpm"
+#include "./bitmaps/block5.xpm"
+#include "./bitmaps/block6.xpm"
+#include "./bitmaps/block1cr.xpm"
+#include "./bitmaps/block2cr.xpm"
+#include "./bitmaps/block3cr.xpm"
+#include "./bitmaps/block4cr.xpm"
+#include "./bitmaps/block5cr.xpm"
+#include "./bitmaps/block6cr.xpm"
+
+#include "./bitmaps/crush0.xpm"
+#include "./bitmaps/crush1.xpm"
+#include "./bitmaps/crush2.xpm"
+#include "./bitmaps/crush3.xpm"
+#include "./bitmaps/crush4.xpm"
 
 static void  createBWPixmaps(GdkDrawable *w, int depth), createColoredPixmaps(GdkDrawable *w, int depth);
 static void  createGCs(GdkDrawable *), createCrushAnimePixmaps(GdkDrawable *w, int depth);
